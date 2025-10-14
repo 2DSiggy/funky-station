2a15a75440abd2c8404b126430072711aefa8a07
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2025-10-14 21:17:23 +0200

    Add StorageRequireAccessSystem

--- Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
+++ Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
@@ -367,6 +367,12 @@ public abstract class SharedStorageSystem : EntitySystem
                 }
                 else
                 {
+                    var ev = new StorageOpenUIAttemptEvent(args.User);
+                    RaiseLocalEvent(uid, ev);
+
+                    if (ev.Cancelled)
+                        return;
+
                     OpenStorageUI(uid, args.User, component, false);
                 }
             }
@@ -529,7 +535,7 @@ public abstract class SharedStorageSystem : EntitySystem
         if (args.Handled || !storageComp.ClickInsert || !CanInteract(args.User, (uid, storageComp), silent: false))
             return;
 
-        var attemptEv = new StorageInteractUsingAttemptEvent();
+        var attemptEv = new StorageInteractUsingAttemptEvent(args.User);
         RaiseLocalEvent(uid, ref attemptEv);
         if (attemptEv.Cancelled)
             return;
